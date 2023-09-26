8ce1d7b897eb1e943420a4cb5741ac1a0fd5eae0
calezz <godzero89@gmail.com>
2023-09-26 20:13:14 +0100

    1

--- src/content.js
+++ src/content.js
@@ -1,9 +1,13 @@
 export function content(){
     const canvas=document.createElement("canvas")
+    const ctx = canvas.getContext("2d")
+    ctx.fillRect(150,0,150,20)
+    ctx.fillRect(0,20,150,20)
     const style ={
-        width:"100vw",
-        height:"100vh",
+        width:"100%",
+        height:"100px",
     }
+
     Object.assign(canvas.style,style)
     return(canvas)
 }
