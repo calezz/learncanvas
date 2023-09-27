f738699ca97c3805b201f4e65e02dce3c0e79eff
calezz <godzero89@gmail.com>
2023-09-27 04:10:55 +0100

    1

--- src/content.js
+++ src/content.js
@@ -1,14 +1,29 @@
-export function content(){
-    const canvas=document.createElement("canvas")
-    const ctx = canvas.getContext("2d")
-    ctx.fillRect(150,0,150,20)
-    ctx.fillRect(0,20,150,20)
-    const style ={
-        width:"100%",
-        height:"100px",
-    }
+export function content() {
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
+    //global variables ?
+  const WIDTH = window.innerWidth
+  const HEIGHT =innerHeight
+    //console.log(HEIGHT,WIDTH)
+  canvas.width = WIDTH
+  canvas.height = HEIGHT
 
-    Object.assign(canvas.style,style)
-    return(canvas)
+let count=0
+  requestAnimationFrame(draw)
+  function draw() {
+
+    count++
+    const display=count%WIDTH
+
+    ctx.clearRect(0,0,WIDTH,HEIGHT)
+    ctx.fillStyle="black"
+    ctx.fillRect(0, 0,display, 20);
+    ctx.fillStyle = "yellow";
+    ctx.fillRect(0, 20, 200, 20);
+    ctx.closePath();
+    ctx.fill();
+    requestAnimationFrame(draw)
+  }
+
+  return canvas;
 }
-
