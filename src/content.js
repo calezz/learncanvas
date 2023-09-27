9d3fafb97e29d0a2e1e8fd3927c04ee873096e8a
calezz <godzero89@gmail.com>
2023-09-27 06:58:51 +0100

    1

--- src/content.js
+++ src/content.js
@@ -1,29 +1,39 @@
 export function content() {
   const canvas = document.createElement("canvas");
   const ctx = canvas.getContext("2d");
-    //global variables ?
-  const WIDTH = window.innerWidth
-  const HEIGHT =innerHeight
-    //console.log(HEIGHT,WIDTH)
-  canvas.width = WIDTH
-  canvas.height = HEIGHT
-
-let count=0
-  requestAnimationFrame(draw)
+  let count = 0;
+  requestAnimationFrame(draw);
+  
   function draw() {
-
-    count++
-    const display=count%WIDTH
-
-    ctx.clearRect(0,0,WIDTH,HEIGHT)
-    ctx.fillStyle="black"
-    ctx.fillRect(0, 0,display, 20);
+      const WIDTH = innerWidth;
+      const HEIGHT = innerHeight;
+      canvas.width = WIDTH;
+      canvas.height = HEIGHT;
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+    drawGrid(HEIGHT,WIDTH)
+    count++;
+    const display = count % WIDTH;
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, display, 16);
     ctx.fillStyle = "yellow";
     ctx.fillRect(0, 20, 200, 20);
     ctx.closePath();
     ctx.fill();
-    requestAnimationFrame(draw)
+    requestAnimationFrame(draw);
   }
+  function drawGrid(HEIGHT,WIDTH){
+
+      const value =16
+    for(let dx=0;dx<WIDTH/value;dx++){
+        for(let dy=0;dy<HEIGHT/value;dy++){
+            ctx.fillStyle=(dx+dy%2)%2===0?"#808080":"#c0c0c0"
+            ctx.fillRect(dx*value,dy*value,value,value)
+        }
+  }}
+
+  let resizeTimeout;
+
+
 
   return canvas;
 }
